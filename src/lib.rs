801dca13fb3125513f9a199c2c29e09b3544530c
rcxid <gc6174@gmail.com>
2025-07-30 23:49:13 +0800

    feat: 实现bing每日一图

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,39 @@
-use axum::{routing::get, Router};
-use tower_service::Service;
 use worker::*;
 
-fn router() -> Router {
-    Router::new().route("/", get(root))
-}
-
 #[event(fetch)]
-async fn fetch(
-    req: HttpRequest,
-    _env: Env,
-    _ctx: Context,
-) -> Result<axum::http::Response<axum::body::Body>> {
+async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
     console_error_panic_hook::set_once();
-    Ok(router().call(req).await?)
+    Router::new()
+        .get_async("/", root)
+        .get_async("/bing", bing)
+        .run(req, env)
+        .await
 }
 
-pub async fn root() -> &'static str {
-    "Hello Workers!"
+async fn root(_: Request, _ctx: RouteContext<()>) -> Result<Response> {
+    Response::ok("Hello Workers!")
+}
+
+async fn bing(_: Request, _ctx: RouteContext<()>) -> Result<Response> {
+    let request = Request::new(
+        "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1",
+        Method::Get,
+    )?;
+    let response = Fetch::Request(request)
+        .send()
+        .await?
+        .json::<serde_json::Value>()
+        .await?;
+
+    let url_base = response
+        .get("images")
+        .and_then(|x| x.get(0))
+        .and_then(|x| x.get("urlbase"))
+        .and_then(|x| x.as_str())
+        .ok_or(Error::Infallible)?;
+
+    let url = format!("https://www.bing.com{url_base}_1920x1080.jpg");
+    let destination_url = Url::parse(url.as_str())?;
+    let status_code = 302;
+    Response::redirect_with_status(destination_url, status_code)
 }
