9b8281de88a4c84368bddff0af35490510b5968b
rcxid <gc6174@gmail.com>
2025-07-31 17:42:21 +0800

    Use tailwindcss.

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,6 @@
 use leptos::prelude::*;
 use leptos::{component, view, IntoView};
-use leptos_meta::{provide_meta_context, MetaTags};
+use leptos_meta::{provide_meta_context, MetaTags, Stylesheet};
 
 #[component]
 pub fn App() -> impl IntoView {
@@ -9,7 +9,9 @@ pub fn App() -> impl IntoView {
 
     // let (value, _set_value) = signal(0);
     view! {
-        <div class="root">Hello Leptos!</div>
+        <div>
+            <div class="text-red-500">Hello Leptos!</div>
+        </div>
     }
 }
 
@@ -20,6 +22,8 @@ pub fn shell(options: LeptosOptions) -> impl IntoView {
             <head>
                 <meta charset="utf-8"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <Stylesheet href="/style/main.css"/>
+                <Stylesheet href="/style/tailwind.css"/>
                 <AutoReload options=options.clone()/>
                 <HydrationScripts options/>
                 <MetaTags/>
